354a87aeebc7a77ccda7d6ecae0ee5e942da5827
Jennifer <ncsujenn@gmail.com>
2014-09-02 21:39:27 -0400

    Added second dial

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
-var position = [0, 36, 72, 108, 144, 180, 216, 252, 288, 324];
-var panels = ["panel0", "panel1", "panel2", "panel3", "panel4", "panel5", "panel6", "panel7", "panel8", "panel9"]; 
+var position10 = [0, 36, 72, 108, 144, 180, 216, 252, 288, 324];
+var position6 = [0, 60, 120, 180, 240, 300];
+var panels10 = ["panel0", "panel1", "panel2", "panel3", "panel4", "panel5", "panel6", "panel7", "panel8", "panel9"]; 
+var panels6 = ["panel0", "panel1", "panel2", "panel3", "panel4", "panel5"]; 
 
 $(document).ready(function(){
 	move();
 	
 	function move(){
-	for(var i = 0; i < panels.length; i++){		
-			position[i] -= 36;		
-	$('#' + panels[i]).css({ transform: 'rotateX(' + position[i] + 'deg ) translateZ( 200px )',
+		setInterval(minute1, 1000);
+		setInterval(minute2, 10000);
+		
+	}
+	
+	function minute1(){
+	for(var i = 0; i < panels10.length; i++){		
+			position10[i] -= 36;		
+	$('#rightMinute .' + panels10[i]).css({ transform: 'rotateX(' + position10[i] + 'deg ) translateZ( 200px )',
 							transition: 'transform .4s linear'});        
 	}
-	setTimeout(move, 1000);
+	}
+	
+	function minute2(){
+	for(var i = 0; i < panels6.length; i++){		
+			position6[i] -= 60;		
+	$('#leftMinute .' + panels6[i]).css({ transform: 'rotateX(' + position6[i] + 'deg ) translateZ( 200px )',
+							transition: 'transform .4s linear'});        
+	}
 	}
 });
  
